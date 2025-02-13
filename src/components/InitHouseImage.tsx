6e2d99da7943ec8c72186fd6058160625d141a6a
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-14 01:52:25 +0900

    fix : init page - text, image size 수정

--- src/components/InitHouseImage.tsx
+++ src/components/InitHouseImage.tsx
@@ -1,7 +1,7 @@
 export default function InitHouseImage() {
   return (
     <div className="cursor-pointer">
-      <img src={"/images/house/AOO_INIT_HOUSE_GRAY.png"} alt="init-home" width={200} height={200} />
+      <img src={"/images/house/AOO_INIT_HOUSE_GRAY.png"} alt="init-home" width={220} height={220} />
     </div>
   );
 }
