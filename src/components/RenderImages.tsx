a56f875bcd7bbbd92812653b80a9d7130e15df4f
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-17 23:50:29 +0900

    fix : 유저 메인 페이지에 보더 이미지+방 이미지들 모두 로드 완료 시 이미지 보여주도록 설정

--- src/components/RenderImages.tsx
+++ src/components/RenderImages.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import API_CONFIG from "../config/api";
 import { UserMainHomeDetail } from "../types/home";
+import SpinnerIcon from "./icons/SpinnerIcon";
+import { useEffect, useState } from "react";
 
 type RenderImagesProps = {
   homeId: number;
@@ -8,15 +10,32 @@ type RenderImagesProps = {
   scale: number;
 };
 
-export default function RenderImages({ homeId, homeData, scale }: RenderImagesProps) {
+export default function RenderImages({ homeId, scale, homeData }: RenderImagesProps) {
+  const totalImages = homeData.rooms.length + 1;
+  const [loadedImages, setLoadedImages] = useState(0);
+  const [isImagesLoaded, setIsImagesLoaded] = useState(false);
+  console.log("loadedImages", loadedImages);
+
+  useEffect(() => {
+    if (loadedImages === totalImages) {
+      setIsImagesLoaded(true);
+    }
+  }, [loadedImages, totalImages]);
+
+  const handleImageLoad = () => {
+    setLoadedImages((prev) => prev + 1);
+  };
+
   return (
     <div className="relative flex justify-center  ">
-      <div className="relative">
+      {!isImagesLoaded && <SpinnerIcon />}
+      <div className={isImagesLoaded ? "relative" : "hidden"}>
         <img
           alt="house-border-image"
           width={window.innerHeight}
           height={window.innerHeight}
           src={`${API_CONFIG.PRIVATE_IMAGE_LOAD_API}/${homeData.house.borderImageId}`}
+          onLoad={handleImageLoad}
         />
 
         {homeData.rooms.map((room) => (
@@ -27,6 +46,7 @@ export default function RenderImages({ homeId, homeData, scale }: RenderImagesPr
               alt={room.name}
               width={Number(room.width) * scale}
               height={Number(room.height) * scale}
+              onLoad={handleImageLoad}
               style={{
                 position: "absolute",
                 left: Number(room.x) * scale,
