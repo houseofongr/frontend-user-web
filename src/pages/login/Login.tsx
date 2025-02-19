d445f80a519b80f0829cdee95be2b9ec52c8ed22
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-20 01:04:17 +0900

    fix : 약관 동의 모두 동의 시 선택항목도 체크 표시

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import API_CONFIG from "../../config/api";
 
 import InitHouseImage from "../../components/InitHouseImage";
-import Footer from "../../components/layout/Footer";
 
 export default function LoginPage() {
   const [isAnimationComplete, setIsAnimationComplete] = useState<boolean>(false);
@@ -15,7 +14,7 @@ export default function LoginPage() {
 
   return (
     <div className="h-screen flex-center">
-      <div className="flex justify-center items-center relative border h-full w-full">
+      <div className="flex justify-center items-center relative  h-full w-full">
         <motion.div
           initial={{ y: 0 }}
           animate={{ y: -50 }}
@@ -47,8 +46,6 @@ export default function LoginPage() {
           </motion.div>
         )}
       </div>
-
-      <Footer />
     </div>
   );
 }
