c80ca09caa21727b5d07f0b34debaf09aed3ae5c
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-23 14:45:19 +0900

    feat : about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,29 @@
 export default function AboutPage() {
-  return <section className="p-10  ">아오옹 소개페이지입니다</section>;
+  return (
+    <section className="flex-center mt-[10%]  ">
+      <div className="">
+        <p className="font-extralight text-lg">우리는 모두 옹알이를 시작으로, 세상을 향한 첫마디를 시작합니다.</p>
+        <br />
+        <br />
+        <div>
+          <strong>아카이브 오브 옹알</strong>은
+        </div>
+        <p>소리는 기억을 가장 생생하게 기록한다. 라는 생각을 시작으로</p>
+        <p>개인의 일상을 소리로 포착하고 적재하여 보존하기 위해 만들어졌습니다.</p>
+        <br />
+        <br />
+        <p>목소리를 녹음하며 나를 기록하고</p>
+        <p>그 기록이 또 다른 누군가에게 오랜 기억의 저장고가 될 수 있도록 돕는</p>
+        <p>[소리 채록] 프로그램과</p>
+        <p>녹음된 소리를 안전하게 저장하고 보존하는</p>
+        <p>[아카이브 플랫폼]을 개발하고 운영합니다.</p>
+        <br />
+        <br />
+        <p>시간을 담는 소리의 집,</p>
+        <div>
+          <strong>아카이브 오브 옹알</strong>의 문을 열어 보세요!
+        </div>
+      </div>
+    </section>
+  );
 }
