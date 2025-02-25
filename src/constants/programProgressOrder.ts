30b63a15d1da446a8acd624fbae59b11bf33bdea
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-26 00:56:01 +0900

    feat: 프로그램 페이지 + 반응형

---
+++ src/constants/programProgressOrder.ts
@@ -0,0 +1,6 @@
+export const programImages = [
+  { src: "/images/program/program_01.PNG", caption: "1. 기록하고 싶은 내용 작성" },
+  { src: "/images/program/program_02.PNG", caption: "2. 소리녹음" },
+  { src: "/images/program/program_03.PNG", caption: "3. 오디오 프로세싱" },
+  { src: "/images/program/program_04.PNG", caption: "4. 클라우드에 보관한 소리 어디서나 듣기" },
+];
--- src/pages/common/Program.tsx
+++ src/pages/common/Program.tsx
@@ -1,3 +1,27 @@
+import { programImages } from "../../constants/programProgressOrder";
+
 export default function ProgramPage() {
-  return <div> 프로그램 페이지</div>;
+  return (
+    <div className="py-10">
+      {/* <p className="text-center text-xl  mb-6">제목</p> */}
+      <ProgramImages images={programImages} />
+    </div>
+  );
 }
+
+type ProgramImageProp = {
+  images: { src: string; caption: string }[];
+};
+
+const ProgramImages = ({ images }: ProgramImageProp) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-10 max-w-2xl mx-auto">
+      {images.map((item, index) => (
+        <div key={index} className="flex flex-col items-center">
+          <img src={item.src} alt={`program-progress-${index + 1}`} className="w-full aspect-square object-cover" />
+          <h1 className="mt-2 text-center text-sm">{item.caption}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
