375fd2a97bb4abbd49628f3d02a3ee495f935282
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-27 00:30:36 +0900

    fix : program page 내용 중앙 위치

--- src/pages/common/Program.tsx
+++ src/pages/common/Program.tsx
@@ -2,8 +2,13 @@ import { programImages } from "../../constants/programProgressOrder";
 
 export default function ProgramPage() {
   return (
-    <div className="py-10">
-      {/* <p className="text-center text-xl  mb-6">제목</p> */}
+    <div className="py-10 w-full flex flex-col items-center ">
+      <section className="w-[60%] ">
+        <span className="text-xl md:text-2xl">프로그램 </span>
+        <p className="text-sm md:text-base font-extralight mb-5">
+          아카이브 오브 옹알의 프로그램 순서는 아래와 같이 진행됩니다.
+        </p>
+      </section>
       <ProgramImages images={programImages} />
     </div>
   );
@@ -15,11 +20,15 @@ type ProgramImageProp = {
 
 const ProgramImages = ({ images }: ProgramImageProp) => {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 gap-2 p-10 max-w-2xl mx-auto">
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 p-10 max-w-2xl mx-auto">
       {images.map((item, index) => (
         <li key={index} className="flex flex-col items-center">
-          <img src={item.src} alt={`program-progress-${index + 1}`} className="w-full aspect-square object-cover" />
-          <h1 className="mt-2 text-center text-sm">{item.caption}</h1>
+          <img
+            src={item.src}
+            alt={`program-progress-${index + 1}`}
+            className="w-1/2 md:w-full aspect-square object-cover"
+          />
+          <p className="mt-2 text-center text-sm">{item.caption}</p>
         </li>
       ))}
     </ul>
