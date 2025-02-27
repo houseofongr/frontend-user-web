17c432be45e3eac9dfb245ab890594e5d6d2cdbe
SUNYOUNG YOON <hh24010016@gmail.com>
2025-02-28 00:16:55 +0900

    fetch test

--- src/components/PreviewContentCopy.tsx
+++ src/components/PreviewContentCopy.tsx
@@ -1,3 +1,4 @@
+import API_CONFIG from "../config/api";
 import WaveformWithAudio from "./Waveform";
 
 type PreviewContentProp = {
@@ -17,7 +18,7 @@ type PreviewContentProp = {
 };
 
 export default function PreviewContentCopy({ data }: { data: PreviewContentProp }) {
-  const { name, description, updatedDate, soundSrc } = data;
+  const { name, description, updatedDate, soundSrc, audioFileId } = data;
   return (
     <div className="flex flex-col md:flex-row gap-5 text-black px-7 min-h-[550px]  md:min-w-[800px] lg:min-w-[1000px]">
       <div className="fixed mt-2 flex  gap-2 items-end">
@@ -52,8 +53,8 @@ export default function PreviewContentCopy({ data }: { data: PreviewContentProp 
 
       {/* 파형 섹션 */}
       <div className="w-1/2 flex-center  ">
-        <WaveformWithAudio audioUrl={soundSrc} audioTitle={name} />
-        {/* <WaveformWithAudio audioUrl={`${API_CONFIG.PUBLIC_AUDIO_LOAD_API}/${audioFileId}`} audioTitle={name} /> */}
+        {/* <WaveformWithAudio audioUrl={soundSrc} audioTitle={name} /> */}
+        <WaveformWithAudio audioUrl={`${API_CONFIG.PUBLIC_AUDIO_LOAD_API}/${audioFileId}`} audioTitle={name} />
       </div>
     </div>
   );
