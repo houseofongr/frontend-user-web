dd039e436a1bc0403f41985917a482824398852c
SUNYOUNG YOON <hh24010016@gmail.com>
2025-03-05 02:59:13 +0900

    fix : contact page css 수정

--- src/pages/common/Contact.tsx
+++ src/pages/common/Contact.tsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom";
 import ContactForm from "../../components/ContactForm";
 import { FOOTER_HEIGHT, HEADER_HEIGHT } from "../../constants/componentSize";
-// import { SiYoutube } from "react-icons/si";
 import { SlSocialInstagram } from "react-icons/sl";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
+import { IoTime } from "react-icons/io5";
 
 const contactInfo = [
-  { icon: <IoMail size={17} color="#f5946d" />, text: "contact@archiveofongr.com" },
-  { icon: <FaPhoneAlt size={15} color="#f5946d" />, text: "010-0000-0000" },
-];
-
-const locationInfo = {
-  icon: <FaLocationDot size={17} color="#f5946d" />,
-  text: "2, Dongil-ro 2-gil, Gwangjin-gu, Seoul Republic of Korea",
-};
-
-const businessHours = [
-  { label: "Week", time: "10:00 - 20:00" },
-  { label: "Lunch", time: "12:00 - 13:00" },
+  { icon: <IoMail size={20} color="#f5946d" />, label: "메일 주소", text: "contact@archiveofongr.com" },
+  { icon: <FaPhoneAlt size={20} color="#f5946d" />, label: "핸드폰", text: "010-0000-0000" },
+  {
+    icon: <FaLocationDot size={20} color="#f5946d" />,
+    label: "위치",
+    text: "2, Dongil-ro 2-gil, Gwangjin-gu, Seoul Republic of Korea",
+  },
+  {
+    icon: <IoTime size={20} color="#f5946d" />,
+    label: "영업 시간",
+    text: "Week : 10:00 - 20:00  /  Lunch : 12:00 - 13:00 ",
+  },
 ];
 
 const snsLinks = [
@@ -29,8 +29,8 @@ const snsLinks = [
     label: "instagram",
   },
   // {
-  //   icon: <SiYoutube size={30} />,
-  //   url: "",
+  //   icon: <SlSocialYoutube size={30} />,
+  //   url: "https://www.instagram.com/houseofongr/?utm_source=ig_web_button_share_sheet",
   //   label: "youtube",
   // },
 ];
@@ -41,58 +41,45 @@ export default function ContactPage() {
       className="w-full flex flex-col bg-neutral-100"
       style={{ minHeight: `calc(100vh - ${HEADER_HEIGHT}px - ${FOOTER_HEIGHT}px)` }}
     >
-      <div className="w-[80%] flex-col md:flex-row flex items-center justify-center m-auto gap-20  py-10">
+      <div className="mx-8 md:mx-auto md:w-[70%] flex-col md:flex-row flex items-center justify-center m-auto gap-20  py-10">
         {/* 왼쪽 섹션 */}
-        <section className="w-full md:w-2/5 flex-col flex gap-6 pl-10">
-          <div>
-            <p className="my-1 font-bold">CONTACT</p>
+        <section className="md:w-1/2 min-h-[600px] flex-col">
+          <p className="text-xl md:text-2xl">문의</p>
+          <p className="text-sm md:text-base font-extralight mb-10">
+            아래의 이메일 주소를 사용하거나 양식을 사용하여 문의 부탁 드립니다.
+          </p>
+
+          <ul className="pl-4">
             {contactInfo.map((item, index) => (
-              <p key={index} className="flex gap-2 items-center font-extralight text-sm py-2">
-                <div className="p-2 rounded-full bg-white">{item.icon}</div>
-                <span>{item.text}</span>
-              </p>
+              <li key={index} className="flex gap-4 items-center py-3">
+                <div className="p-3 rounded-full bg-white">{item.icon}</div>
+                <div className="flex flex-col leading-tight">
+                  <label className="text-[10px] md:text-xs">{item.label}</label>
+                  <span className="font-extralight text-sm md:text-base">{item.text}</span>
+                </div>
+              </li>
             ))}
-          </div>
-
-          {/* location info */}
-          <div>
-            <p className="my-1 font-bold">LOCATION</p>
-            <p className="flex gap-2 items-center font-extralight text-sm py-2">
-              <div className="p-2 rounded-full bg-white">{locationInfo.icon}</div>
-              <span>{locationInfo.text}</span>
-            </p>
-          </div>
-
-          {/* business hours info */}
-          <div>
-            <p className="my-1 font-bold">BUSINESS HOURS</p>
-            <div className="pl-2 text-sm font-extralight">
-              {businessHours.map((item, index) => (
-                <p key={index}>
-                  ・ {item.label} {item.time}
-                </p>
-              ))}
-            </div>
-          </div>
-
-          {/* sns info */}
-          <div>
-            <p className="my-1 font-bold">SNS</p>
-            <div className=" flex  p-2 gap-4">
-              {snsLinks.map((item, index) => (
-                <Link key={index} to={item.url} target="_blank" className="max-w-[80px] flex-center flex-col">
-                  {item.icon}
-                  <p className="text-[10px] font-extralight">{item.label}</p>
-                </Link>
-              ))}
-            </div>
+          </ul>
+          <div className="py-10">
+            <p className="text-sm md:text-base font-extralight">SNS</p>
+            <ul className="p-4 flex gap-2">
+              {snsLinks.map((sns, index) => {
+                return (
+                  <li key={index}>
+                    <Link to={sns.url} target="_blank" className="flex-center flex-col">
+                      <div>{sns.icon}</div>
+                      <label className="text-xs">{sns.label}</label>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </section>
         {/* 오른쪽 섹션 */}
-        <section className="md:w-1/2 lg:w-2/5 shadow-md flex-col flex p-2 lg:p-7 bg-white rounded">
-          <div className="m-4">
-            <p className="text-2xl font-extrabold py-1">CONTACT US</p>
-            <p className="text-sm md:text-base font-extralight mb-5">
+        <section className="md:w-1/2 shadow-md flex-col flex p-3 lg:p-7 bg-white rounded">
+          <div>
+            <p className="text-sm md:text-base font-extralight ">
               '아카이브 오브 옹알'에 궁금한 점이나 문의 사항이 있다면 아래 양식을 작성해 주세요.
             </p>
           </div>
