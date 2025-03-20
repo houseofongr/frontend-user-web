c4e0b1eb41c16c30fc78eecd9b7976d9edd5f4e8
SUNYOUNG YOON <hh24010016@gmail.com>
2025-03-20 18:15:11 +0900

    fix : 줄바꿈 대응 위한 모달 메모지 스타일 변경

--- src/components/PreviewContent.tsx
+++ src/components/PreviewContent.tsx
@@ -14,6 +14,7 @@ type PreviewContent = {
 
 export default function PreviewContent({ data }: PreviewContent) {
   const { name, description, audioFileId, updatedDate } = data;
+  // console.log("description길이", description.length);
 
   return (
     <div className="flex flex-col gap-5 text-black min-h-[400px] sm:min-h-[550px] min-w-[330px] md:min-w-[500px] lg:min-w-[900px]">
@@ -25,32 +26,37 @@ export default function PreviewContent({ data }: PreviewContent) {
       <div className="pt-0 flex flex-col lg:flex-row">
         <div className="w-full lg:w-1/2 relative flex-center ">
           <div
-            className="absolute bg-cover bg-center w-[300px] h-[280px] md:w-[400px] md:h-[400px]"
+            className="absolute bg-cover bg-center w-[300px] h-[300px] md:w-[420px] md:h-[400px]"
             style={{
               backgroundImage: 'url("/images/notepad/notepad_5.png")',
             }}
           />
-          <div className="relative flex items-center text-gray-700 w-[250px] md:w-[400px] min-h-[280px] md:h-[400px]">
-            <div className="flex flex-col w-full  md:px-10">
+          <div className="relative flex items-center text-gray-700 w-[250px] md:w-[400px] min-h-[300px] md:h-[400px]">
+            <div className={`flex flex-col w-full ${description.length > 230 ? "md:px-5" : "md:px-10 "} `}>
               <h2
-                className="leading-4.5 md:leading-6 text-2xl md:text-3xl pt-10"
+                className={`leading-4.5 md:leading-6 text-2xl md:text-3xl ${
+                  description.length > 230 ? "pt-10" : "pt-10 pb-3"
+                } `}
                 style={{ fontFamily: "SangSangShinb7" }}
               >
                 제목 : {name}
               </h2>
-              <p
-                className="leading-4.5 md:leading-6 text-lg md:text-2xl min-h-[100px] max-h-[300px] md:max-h-[200px]  pt-3 "
-                style={{ fontFamily: "SangSangShinb7" }}
+              <div
+                className={`leading-4 md:leading-5.5 text-lg md:text-2xl min-h-[120px] max-h-fit ${
+                  description.length > 230 ? "md:max-h-fit" : "md:max-h-[260px] "
+                } pt-3 `}
+                style={{ fontFamily: "SangSangShinb7", whiteSpace: "pre-wrap" }}
               >
                 {description}
                 {/* description - 255자 테스트 */}
                 {/* 도자기를 굽는 데는 1,000°C 이상의 고온이 필요한데, 이를 위한 장치가 가마라 불리는 것으로, 최소한의
                 연료를 사용하여 최고의 온도를 얻을 수 있도록 고안되어 있다. 도자기를 굽는 데는 1,000°C 이상의 고온이
                 필요한데, 이를 위한 장치가 가마라 불리는 것으로, 최소한의 연료를 사용하여 최고의 온도를 얻을 수 있도록
-                고안되어 있다.. 도자기를 굽는 데는 1,000°C 이상의 고온이 필요한데,이를 위한 장치가 가마라 불리는 것으로,
+                고안되어 있다.. 도자기를 
+                굽는 데는 1,000°C 이상의 고온이 필요한데,이를 위한 장치가 가마라 불리는 것으로,
                 최소한의연료. */}
-              </p>
-              <p className="text-right md:text-2xl p-2" style={{ fontFamily: "SangSangShinb7" }}>
+              </div>
+              <p className="text-right md:text-2xl pb-3" style={{ fontFamily: "SangSangShinb7" }}>
                 {formatDate(updatedDate)}
               </p>
             </div>
